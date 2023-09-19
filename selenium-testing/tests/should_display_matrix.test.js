7c47c28767111638cef257f836b53dd60db2734d
nelsonfr <nelsonfabianrios@hotmail.com>
2023-09-19 12:51:58 -0500

    Adds tests that checks if matrix C is displayed

--- selenium-testing/tests/should_display_matrix.test.js
+++ selenium-testing/tests/should_display_matrix.test.js
@@ -12,6 +12,7 @@ describe('Should display matrices', function(){
   after(async function(){
     await driver.quit();
   });
+  
   /**
    * TESTS START HERE
    */
@@ -44,6 +45,38 @@ describe('Should display matrices', function(){
     assert.ok(matrixElementA, 'The matrix A was successfully created');
     const matrixElementB = await driver.findElement(By.id('matrixB12'));    
     assert.ok(matrixElementB, 'The matrix B was successfully created');
+
+  });
+
+  it('Should display matrix C', async function(){
+    // Navigate to a website
+    await driver.get('http://localhost:4200/');
+
+    //Matrix A
+    const selectHeightA = await driver.findElement(By.id('matrixAHeight'));
+    const heightA = await new webdriver.Select(selectHeightA);
+    await heightA.selectByValue('3');//Selects matrix a with 3 rows
+
+    const selectWidthA = await  driver.findElement(By.id('matrixAWidth'));
+    const widthA = await new webdriver.Select(selectWidthA);
+    await widthA.selectByValue('2'); //Selects matrix a with 2 cols
+
+    //Matrix B
+    const selectHeightB = await driver.findElement(By.id('matrixBHeight'));
+    const heightB = await new webdriver.Select(selectHeightB);
+    await heightB.selectByValue('2');//Selects matrix a with 2 rows
+
+    const selectWidthB = await  driver.findElement(By.id('matrixBWidth'));
+    const widthB = await  new webdriver.Select(selectWidthB);
+    await widthB.selectByValue('3'); //Selects matrix a with 3 cols
+
+    const button = await driver.findElement(By.id('multiplyBtn'));
+    button.click();
+    
+    await driver.wait(until.elementLocated(By.id('output-container')), 5000);
+
+    const matrixElementC = await driver.findElement(By.id('matrixC22'));
+    assert.ok(matrixElementC, 'The matrix C was successfully created');
 
   });
 
--- src/app/read-only-matrix-component/read-only-matrix-component.component.ts
+++ src/app/read-only-matrix-component/read-only-matrix-component.component.ts
@@ -7,4 +7,5 @@ import { Component, Input } from '@angular/core';
 })
 export class ReadOnlyMatrixComponentComponent {
   @Input() matrixData:number[][];
+  @Input() matrixName:string;
 }
